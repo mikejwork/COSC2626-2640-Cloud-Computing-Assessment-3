28dd6dfe7ee5a20960c11589737ecac507ebcea9
Mike <mikejurie.work@gmail.com>
2021-05-26 20:47:47 +1000

    Image fix

--- templates/template.php
+++ templates/template.php
@@ -13,7 +13,7 @@
     {% block head %} {% endblock %}
 </head>
 
-<body style="background: linear-gradient(rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.5)), url(&quot;assets/img/99a9d89b174548f4c6be1462c06ae6a9.jpg&quot;) center / cover;">
+<body style="background: linear-gradient(rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.5)), url(&quot;https://i.imgur.com/BxYs9Tw.jpg&quot;) center / cover;">
     <nav class="navbar navbar-light navbar-expand-md" style="background: #121212;font-family: Roboto, sans-serif;border-bottom-color: rgb(255,255,255);padding-right: 10%;padding-left: 10%;">
         <div class="container-fluid"><a class="navbar-brand" href="#" style="color: rgb(225,225,225);font-family: Ubuntu, sans-serif;">portfol.io</a>
             <div class="collapse navbar-collapse" id="navcol-1">
