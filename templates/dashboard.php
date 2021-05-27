61d86c096ba8d4ac4f9a0b338bfcbef505bf1eb0
Mike <mikejurie.work@gmail.com>
2021-05-27 14:49:20 +1000

    User picks on dashboard

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -19,7 +19,8 @@
         </div>
         <div class="container">
             <div class="row g-0 d-xxl-flex justify-content-center align-items-center align-items-xxl-center">
-                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
                     <div style="display: flex;margin-bottom: 5px;">
                         <p style="margin-bottom: 0px;">BTC $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
                         <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
@@ -34,7 +35,8 @@
                     </div>
                     <div style="margin: 0px;height: 100px;"></div>
                 </div>
-                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+                
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
                     <div style="display: flex;margin-bottom: 5px;">
                         <p style="margin-bottom: 0px;">ADA $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
                         <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
@@ -49,7 +51,8 @@
                     </div>
                     <div style="margin: 0px;height: 100px;"></div>
                 </div>
-                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
                     <div style="display: flex;margin-bottom: 5px;">
                         <p style="margin-bottom: 0px;">ETH $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
                         <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
@@ -64,7 +67,8 @@
                     </div>
                     <div style="margin: 0px;height: 100px;"></div>
                 </div>
-                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
                     <div style="display: flex;margin-bottom: 5px;">
                         <p style="margin-bottom: 0px;">XRP $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
                         <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
@@ -78,6 +82,59 @@
                         </div>
                     </div>
                     <div style="margin: 0px;height: 100px;"></div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="container" style="color: rgb(255,255,255);padding: 2%;background: rgba(18,18,18,0);border-radius: 15px;font-family: Roboto, sans-serif;padding-bottom: 0px;padding-left: 10%;padding-right: 10%;">
+            <div style="margin-bottom: 5%;">
+                <h1 style="font-weight: normal;font-style: normal;font-family: Ubuntu, sans-serif;margin-bottom: 5px;">user insight</h1>
+                <p style="color: rgba(255,255,255,0.5);">View the most popular stocks being purchased by our userbase.</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row g-0 d-xxl-flex justify-content-center align-items-center align-items-xxl-center">
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;">BTC $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
+                        <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgba(40,224,24,0.85);"><i class="fa fa-long-arrow-up" style="margin-right: 5px;"></i>24hr Highest mover</p>
+                        </div>
+                    </div>
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;color: rgba(48,105,217,0.85);"><br></p>
+                        <div style="width: auto;background: rgba(14,14,14,0);display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgb(71,71,71);border-color: rgb(18,18,18);"><strong>$48,238.80</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;">ADA $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
+                        <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgba(40,224,24,0.85);"><i class="fa fa-long-arrow-up" style="margin-right: 5px;"></i>24hr Highest gainer</p>
+                        </div>
+                    </div>
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;color: rgba(48,105,217,0.85);"><br></p>
+                        <div style="width: auto;background: rgba(14,14,14,0);display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgb(71,71,71);border-color: rgb(18,18,18);"><strong>$2.16</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto col-md-3" style="color: rgba(255,255,255,0.85);background: #191919DD;font-family: Ubuntu, sans-serif;border-radius: 10px;padding: 15px;padding-right: 15px;padding-left: 15px;margin-right: 5px;margin-bottom: 5px;">
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;">XRP $ AUD<i class="fa fa-question-circle" style="margin-left: 5px;"></i></p>
+                        <div style="width: auto;background: #0e0e0e;display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgba(40,224,24,0.85);"><i class="fa fa-long-arrow-up" style="margin-right: 5px;"></i>Portfol.io user pick</p>
+                        </div>
+                    </div>
+                    <div style="display: flex;margin-bottom: 5px;">
+                        <p style="margin-bottom: 0px;color: rgba(48,105,217,0.85);"><br></p>
+                        <div style="width: auto;background: rgba(14,14,14,0);display: inline-block;border-radius: 20px;padding-right: 12px;padding-left: 12px;margin-left: auto;">
+                            <p style="margin-bottom: 0px;color: rgb(71,71,71);border-color: rgb(18,18,18);"><strong>$1.37</strong></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
