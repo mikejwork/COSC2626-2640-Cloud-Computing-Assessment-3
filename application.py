9bb0b23e17b12fc97400e0003d9451e37db7939d
Mike <mikejurie.work@gmail.com>
2021-05-28 13:57:29 +1000

    Minor fixes.

--- application.py
+++ application.py
@@ -7,9 +7,18 @@ from boto3.dynamodb.conditions import Key, Attr
 from flask import Flask, render_template, request, redirect, url_for, session
 application = Flask(__name__)
 app = application
-app.secret_key = "iqFfhY9FCUOJ8Z46DQLDe93mEMBln4W6"
-dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key_id="AKIAQMIDYSWEMRZJYWVT", aws_secret_access_key="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")
-apigateway_client = boto3.client('apigateway', region_name='us-east-1', aws_access_key_id="AKIAQMIDYSWEMRZJYWVT", aws_secret_access_key="IvLXb/dIYL3Bg7p7Z4DCx4HkiodeLWfqhnpC3GpN")
+
+# Change these according to your details
+variables = {
+    "secret_key": "iqFfhY9FCUOJ8Z46DQLDe93mEMBln4W6",
+    "region_name": "us-east-1",
+    "aws_access_key_id": "AKIAQMIDYSWEMRZJYWVT",
+    "aws_secret_access_key": "IvLXb/dIYL3Bg7p7Z4DCx4HkiodeLWfqhnpC3GpN"
+}
+
+app.secret_key = variables["secret_key"]
+dynamodb_resource = boto3.resource('dynamodb', region_name=variables["region_name"], aws_access_key_id=variables["aws_access_key_id"], aws_secret_access_key=variables["aws_secret_access_key"])
+apigateway_client = boto3.client('apigateway', region_name=variables["region_name"], aws_access_key_id=variables["aws_access_key_id"], aws_secret_access_key=variables["aws_secret_access_key"])
 
 # Functions
 def auth_login(email, password):
